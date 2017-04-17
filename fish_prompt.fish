d7d0b5a223ab0af2ece3b3d2d4b0a4a40efd0c39
Jonas Damtoft <jonasdamtoft@hotmail.com>
2017-04-17 16:22:21 +0200

    Add relative path in git projects

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,15 @@
+set -q jomik_prompt_arrow_glyph
+or set -g jomik_prompt_arrow_glyph ""
+
+set -q jomik_project_root_glyph
+or set -g jomik_project_root_glyph "⊤"
+
+set -q jomik_git_branch_glyph
+or set -g jomik_git_branch_glyph ""
+
 function __jomik_prompt_segment
   set_color $argv[1]
-  echo -nse "[" $argv[2..-1] "]"
+  echo -ns "[" $argv[2..-1] "]"
   set_color normal
 end
 
@@ -33,35 +42,52 @@ function __jomik_git_project_name
 end
 
 function __jomik_git_relative_path
-  echo (command realpath --relative-to=(__jomik_git_root) $PWD)
+  set -l rel (command realpath --relative-to=(__jomik_git_root) $PWD)
+  if test "$rel" != "."
+    echo -ns "/" $rel
+  end
 end
 
 function __jomik_prompt_git
   set -l git_branch (__jomik_git_branch_name)
   if test $git_branch
     set -l proj_name (__jomik_git_project_name)
-    __jomik_prompt_segment blue $proj_name " @ " $git_branch
+    __jomik_prompt_segment blue $proj_name " " $jomik_git_branch_glyph " " $git_branch
   end
 end
 
 function __jomik_prompt_dir
   if test (__jomik_git_branch_name)
     set -l rel_path (__jomik_git_relative_path)
-    __jomik_prompt_segment cyan (__jomik_path_to_prompt_fit $rel_path)
+    __jomik_prompt_segment cyan $jomik_project_root_glyph (__jomik_path_to_prompt_fit $rel_path)
   else
     __jomik_prompt_segment cyan (prompt_pwd)
   end
 end
 
+function __jomik_prompt_error
+  if test "$last_status" -ne 0
+    __jomik_prompt_segment red $last_status
+  end
+end
+
 function __jomik_prompt_arrow
-  echo -n ">"
+  echo -ns $jomik_prompt_arrow_glyph " "
+end
+
+function __jomik_prompt_status_line
+  type -q git; and __jomik_prompt_git
 end
 
 function fish_prompt
-  type -q git; and __jomik_prompt_git
-  echo -nse "\n"
-  __jomik_prompt_dir
-  __jomik_prompt_arrow
+  set -g last_status $status
 
-  set_color normal
+  set -l status_line (__jomik_prompt_status_line)
+  if test $status_line
+    echo $status_line
+  end
+
+  __jomik_prompt_dir
+  __jomik_prompt_error
+  __jomik_prompt_arrow
 end
