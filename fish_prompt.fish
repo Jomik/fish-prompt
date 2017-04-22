bd8a21cf92ad00e19dd942fef238eae95910f0dc
Jonas Damtoft <jonasdamtoft@hotmail.com>
2017-04-22 14:40:48 +0200

    Change glyphs

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,10 +1,10 @@
 set -q jomik_prompt_arrow_glyph
-or set -g jomik_prompt_arrow_glyph ""
+or set -g jomik_prompt_arrow_glyph "➤"
 
 function __jomik_prompt_segment
-  set_color $argv[1]
-  echo -ns "[" $argv[2..-1] "]"
-  set_color normal
+    set_color $argv[1]
+    echo -ns "[" $argv[2..-1] "]"
+    set_color normal
 end
 
 function __jomik_path_to_prompt_fit
@@ -23,52 +23,52 @@ function __jomik_path_to_prompt_fit
 end
 
 function __jomik_in_git_dir
-  if type -q git
-    command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-  else
-    return 1
-  end
+    if type -q git
+        command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    else
+        return 1
+    end
 end
 
 function __jomik_git_root
-  echo (command git rev-parse --show-toplevel ^/dev/null)
+    echo (command git rev-parse --show-toplevel ^/dev/null)
 end
 
 function __jomik_git_project_name
-  echo (command basename (__jomik_git_root))
+    echo (command basename (__jomik_git_root))
 end
 
 function __jomik_git_relative_path
-  set -l rel (command realpath --relative-to=(__jomik_git_root) $PWD)
-  if test "$rel" != "."
-    echo -s "/" $rel
-  end
+    set -l rel (command realpath --relative-to=(__jomik_git_root) $PWD)
+    if test "$rel" != "."
+        echo -s "/" $rel
+    end
 end
 
 function __jomik_prompt_dir
-  if __jomik_in_git_dir
-    set -l rel_path (__jomik_git_relative_path)
-    set -l proj_name (__jomik_git_project_name)
-    __jomik_prompt_segment cyan $proj_name (__jomik_path_to_prompt_fit $rel_path)
-  else
-    __jomik_prompt_segment cyan (prompt_pwd)
-  end
+    if __jomik_in_git_dir
+        set -l rel_path (__jomik_git_relative_path)
+        set -l proj_name (__jomik_git_project_name)
+        __jomik_prompt_segment cyan $proj_name (__jomik_path_to_prompt_fit $rel_path)
+    else
+        __jomik_prompt_segment cyan (prompt_pwd)
+    end
 end
 
 function __jomik_prompt_error
-  if test "$last_status" -ne 0
-    __jomik_prompt_segment red $last_status
-  end
+    if test "$last_status" -ne 0
+        __jomik_prompt_segment red $last_status
+    end
 end
 
 function __jomik_prompt_arrow
-  echo -ns $jomik_prompt_arrow_glyph " "
+    echo -ns $jomik_prompt_arrow_glyph " "
 end
 
 function fish_prompt
-  set -g last_status $status
+    set -g last_status $status
 
-  __jomik_prompt_dir
-  __jomik_prompt_error
-  __jomik_prompt_arrow
+    __jomik_prompt_dir
+    __jomik_prompt_error
+    __jomik_prompt_arrow
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,7 +1,7 @@
 # Credit to https://gist.github.com/britishtea/39ad478fa5180e1432a2
 
 set -q jomik_git_branch_glyph
-or set -g jomik_git_branch_glyph ""
+or set -g jomik_git_branch_glyph "⎇ "
 
 set -q jomik_git_push_glyph
 or set -g jomik_git_push_glyph "⇡"
@@ -10,58 +10,62 @@ set -q jomik_git_pull_glyph
 or set -g jomik_git_pull_glyph "⇣"
 
 function fish_right_prompt
-  # If is in git dir
-  if type -q git; and command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    # Branch name or revision hash
-    set -l branch (git symbolic-ref --short HEAD ^/dev/null; or git show-ref --head -s --abbrev | head -n1 ^/dev/null)
-    set_color blue
-    echo -n "[$branch"
-    
-    command git diff-files --quiet --ignore-submodules ^/dev/null; or set -l has_unstaged_files
-    command git diff-index --quiet --ignore-submodules --cached HEAD ^/dev/null; or set -l has_staged_files
+    # If is in git dir
+    if type -q git
+        and command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        # Branch name or revision hash
+        set -l branch (git symbolic-ref --short HEAD ^/dev/null; or git show-ref --head -s --abbrev | head -n1 ^/dev/null)
+        set_color blue
+        echo -n "["
+        command git diff-files --quiet --ignore-submodules ^/dev/null
+        or set -l has_unstaged_files
+        command git diff-index --quiet --ignore-submodules --cached HEAD ^/dev/null
+        or set -l has_staged_files
 
-    if set -q has_unstaged_files
-      set_color red
-      echo -ns " " $jomik_git_branch_glyph
-    else if set -q has_staged_files
-      set_color yellow
-      echo -ns " " $jomik_git_branch_glyph
-    end
-    
-    command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1; and set -l has_upstream
-    if set -q has_upstream
-      set -l commit_counts (command git rev-list --left-right --count 'HEAD...@{upstream}' ^/dev/null)
-      
-      set -l commits_to_push (echo $commit_counts | cut -f 1 ^/dev/null)
-      set -l commits_to_pull (echo $commit_counts | cut -f 2 ^/dev/null)
-
-      if test $commits_to_push -ne 0
-        if test $commits_to_pull -ne 0
-          set_color red
-        else
-          set_color yellow
+        if set -q has_unstaged_files
+            set_color red
+        else if set -q has_staged_files
+            set_color yellow
         end
-        echo -ns " " $jomik_git_push_glyph
-      end
+        echo -ns $jomik_git_branch_glyph
+        echo -n " $branch"
 
-      if test $commits_to_pull -ne 0
-        if test $commits_to_push -ne 0
-          set_color red
-        else
-          set_color yellow
+
+        command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1
+        and set -l has_upstream
+        if set -q has_upstream
+            set -l commit_counts (command git rev-list --left-right --count 'HEAD...@{upstream}' ^/dev/null)
+
+            set -l commits_to_push (echo $commit_counts | cut -f 1 ^/dev/null)
+            set -l commits_to_pull (echo $commit_counts | cut -f 2 ^/dev/null)
+
+            if test $commits_to_push -ne 0
+                if test $commits_to_pull -ne 0
+                    set_color red
+                else
+                    set_color yellow
+                end
+                echo -ns " $jomik_git_push_glyph"
+            end
+
+            if test $commits_to_pull -ne 0
+                if test $commits_to_push -ne 0
+                    set_color red
+                else
+                    set_color yellow
+                end
+                echo -ns " $jomik_git_pull_glyph"
+            end
         end
-        echo -ns " " $jomik_git_pull_glyph
-      end
+
+        set -l untracked (count (git ls-files --others --exclude-standard ^/dev/null))
+        if test $untracked -ne 0
+            set_color purple
+            echo -ns " ($untracked)"
+        end
+
+        set_color blue
+        echo -n "]"
+        set_color normal
     end
-
-  set -l untracked (count (git ls-files --others --exclude-standard ^/dev/null))
-  if test $untracked -ne 0
-    set_color purple
-    echo -ns " (" $untracked ")"
-  end
-
-  set_color blue
-  echo -n "]"
-  set_color normal
-  end
 end
