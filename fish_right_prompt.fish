44b518436744d7dfe089f7e65ca1ff06b913c898
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-01-26 18:19:04 +0100

    Run git fetch disowned

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -37,7 +37,7 @@ function fish_right_prompt
             # Get upstream and current branch
             set -l origin_current (string split '/' (command git rev-parse --symbolic-full-name --abbrev-ref '@{upstream}'))
             # Slightly hacky?
-            test -n (command git fetch $origin_current >/dev/null ^&1 &)
+            command echo | fish -c 'git fetch $origin_current & disown (jobs -p)'
 
             set -l commit_counts (command git rev-list --left-right --count 'HEAD...@{upstream}' ^/dev/null)
             set -l commits_to_push (echo $commit_counts | cut -f 1 ^/dev/null)
